0f0ba5faa88095b0691fa3055824cd2658af2ae4
326eunjin <326eunjin@naver.com>
2022-02-22 17:27:39 +0900

    without libft

---
+++ include/ft_printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ejang < ejang@student.42seoul.kr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/22 17:15:02 by ejang             #+#    #+#             */
+/*   Updated: 2022/02/22 17:16:59 by ejang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+int		ft_printf(const char *, ...);
+
+#endif
