0a07ba6df335dc31f9851e4e1eb879e9aa7bb486
326eunjin <50578785+326eunjin@users.noreply.github.com>
2022-06-05 19:53:41 +0900

    [ADD] fun_echo

--- built_in/func_echo.c
+++ built_in/func_echo.c
@@ -1,25 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   func_pwd.c                                         :+:      :+:    :+:   */
+/*   func_echo.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ejang <ejang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/05 16:37:21 by ejang             #+#    #+#             */
-/*   Updated: 2022/06/05 17:02:35 by ejang            ###   ########.fr       */
+/*   Created: 2022/06/05 17:04:10 by ejang             #+#    #+#             */
+/*   Updated: 2022/06/05 19:53:28 by ejang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	*fun_pwd(void)//현재 작업중인 절대경로 출력
-{
-	char *current_dir = getcwd(NULL, 0);
-	if (current_dir == NULL)//리턴값이 null이니까 free할 필요가 없음.
-	{
-		printf("getcwd() cannot excute in fun_pwd\n");
-		exit(1);
-	}
-	printf("%s\n",current_dir);
-	free(current_dir);
-}
+
