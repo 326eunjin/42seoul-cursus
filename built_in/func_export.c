e5cc63919f5b58ae8d32804eaf2e308750d4e338
326eunjin <50578785+326eunjin@users.noreply.github.com>
2022-06-06 11:03:36 +0900

    [FIX] main 인자 변경, [ADD] copy_env.c

--- built_in/func_export.c
+++ built_in/func_export.c
@@ -1,30 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   func_export.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ejang <ejang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/06/05 16:33:03 by jeyoon            #+#    #+#             */
-/*   Updated: 2022/06/06 11:02:25 by ejang            ###   ########.fr       */
+/*   Created: 2022/06/05 20:07:54 by ejang             #+#    #+#             */
+/*   Updated: 2022/06/05 20:08:06 by ejang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-int main(int argc, char **argv, char **envp)
-{
-    t_cmd_line *cmd_line;
-    //환경 변수 불러오기
-    while (1)
-    {
-        if (parse_cmd(cmd_line) == FALSE)
-        {
-            //return_error(cmd_line);
-            continue;
-        }
-        // 커맨드라인 유효성 검증 부분
-        // 실행부분
-    }
-    return (0);
-}
