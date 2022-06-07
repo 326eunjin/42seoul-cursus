48acee5a6819d86f764136a69ed8ef9386ef8c29
yoouyeon <jyeon.yoon59@gmail.com>
2022-06-07 19:36:09 +0900

    [REFACTOR] 함수 이동

--- parse/parse_analyze.c
+++ parse/parse_analyze.c
@@ -6,7 +6,7 @@
 /*   By: jeyoon <jeyoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/07 14:28:38 by jeyoon            #+#    #+#             */
-/*   Updated: 2022/06/07 15:52:18 by jeyoon           ###   ########seoul.kr  */
+/*   Updated: 2022/06/07 18:59:14 by jeyoon           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,7 @@
 		- bulitin command : BUILTIN
 		- 그 외 : 일단은 ARG
 	static int	is_bulitin(char *cmd)	:	cmd가 bulitin 함수인지 확인
-	static int	ft_strcmp(char *s1, char *s2)	:	단순 ft_strcmp. (libft에는 ft_strncmp가 있다.)
 */
-
-static int	ft_strcmp(char *s1, char *s2)
-{
-	int	i;
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
-		i++;
-	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
-}
 
 static int	is_bulitin(char *cmd)
 {
