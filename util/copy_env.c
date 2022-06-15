6d771722c3226c372e7b6813b0b9dfad1970c5f4
326eunjin <326eunjin@naver.com>
2022-06-15 17:44:55 +0900

    [FIX] malloc에러처리

--- util/copy_env.c
+++ util/copy_env.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   copy_env.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ejang <ejang@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: ejang <ejang@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 16:06:07 by ejang             #+#    #+#             */
-/*   Updated: 2022/06/15 13:22:08 by ejang            ###   ########.fr       */
+/*   Updated: 2022/06/15 16:27:59 by ejang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,8 @@ char **copy_envp(char **envp)
 	while(envp[++i])
 		cnt++;
 	ret = (char **)malloc(sizeof(char *) * (cnt + 1));
+	if (!ret)
+		return (NULL);
 	i = -1;
 	while(envp[++i])
 		ret[i] = ft_strdup(envp[i]);
