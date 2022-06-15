e24067ff769b49b29e7cf33799b5b8b5a39d7478
yoouyeon <jyeon.yoon59@gmail.com>
2022-06-15 21:12:47 +0900

    [FEAT] ft_strjoin 내 사용한 문자열들 free 추가

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ejang <ejang@student.42seoul.kr>           +#+  +:+       +#+        */
+/*   By: jeyoon <jeyoon@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/24 17:06:29 by ejang             #+#    #+#             */
-/*   Updated: 2022/06/15 13:20:15 by ejang            ###   ########.fr       */
+/*   Updated: 2022/06/15 21:10:00 by jeyoon           ###   ########seoul.kr  */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,5 +27,9 @@ char	*ft_strjoin(char *s1, char *s2)
 		return (NULL);
 	ft_strlcpy(ptr, s1, len1 + 1);
 	ft_strlcpy(ptr + len1, s2, len2 + 1);
+	free(s1);
+	free(s2);
+	s1 = 0;
+	s2 = 0;
 	return (ptr);
 }
