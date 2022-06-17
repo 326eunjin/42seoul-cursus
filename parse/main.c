8783189c45f1e1be77c7a93b12d4b420e2e7ee2a
326eunjin <326eunjin@naver.com>
2022-06-17 17:59:42 +0900

    [ADD] makefile 다시 추가

--- parse/main.c
+++ parse/main.c
@@ -6,7 +6,7 @@
 /*   By: ejang <ejang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/05 16:33:03 by jeyoon            #+#    #+#             */
-/*   Updated: 2022/06/17 16:47:57 by ejang            ###   ########.fr       */
+/*   Updated: 2022/06/17 17:59:14 by ejang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,9 +62,9 @@ int main(int argc, char **argv, char **envp)
 		//exe_builtin(cmd_line_list->cmd_heads[0]);
 		// // *** 디버깅용 프린트 (OLDPWD, PWD)
 		int i = -1;
-		while(g_state.envp[++i])
-			if(ft_strncmp(g_state.envp[i],"OLDPWD",6) == 0 || ft_strncmp(g_state.envp[i],"PWD",3)== 0)
-				printf("%s\n",g_state.envp[i]);
+		// while(g_state.envp[++i])
+		// 	if(ft_strncmp(g_state.envp[i],"OLDPWD",6) == 0 || ft_strncmp(g_state.envp[i],"PWD",3)== 0)
+		// 		printf("%s\n",g_state.envp[i]);
 		// // *** 끝
 		free_cmd(cmd_line_list);
 	}
