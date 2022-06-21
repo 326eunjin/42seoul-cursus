36ba0267e35177afc8f499f42c89423ff158ff12
326eunjin <326eunjin@naver.com>
2022-06-21 11:09:21 +0900

    [FIX]util norm

--- util/copy_env.c
+++ util/copy_env.c
@@ -6,24 +6,27 @@
 /*   By: ejang <ejang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/06 16:06:07 by ejang             #+#    #+#             */
-/*   Updated: 2022/06/16 15:56:54 by ejang            ###   ########.fr       */
+/*   Updated: 2022/06/21 11:08:23 by ejang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-char **copy_envp(char **envp)
+char	**copy_envp(char **envp)
 {
-	int i = -1;
-	int cnt = 0;
-	char **ret;
-	while(envp[++i])
+	int		i;
+	int		cnt;
+	char	**ret;
+
+	i = -1;
+	cnt = 0;
+	while (envp[++i])
 		cnt++;
 	ret = (char **)malloc(sizeof(char *) * (cnt + 1));
 	if (!ret)
 		return (NULL);
 	i = -1;
-	while(envp[++i])
+	while (envp[++i])
 		ret[i] = ft_strdup(envp[i]);
 	ret[cnt] = NULL;
 	return (ret);
--- util/free_split.c
+++ util/free_split.c
@@ -6,20 +6,20 @@
 /*   By: ejang <ejang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/17 17:08:49 by ejang             #+#    #+#             */
-/*   Updated: 2022/06/17 17:11:51 by ejang            ###   ########.fr       */
+/*   Updated: 2022/06/21 11:07:34 by ejang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minishell.h"
 
-void	free_split(char **tmp)//ft_split으로 할당한거 해제하기 
+void	free_split(char **tmp)
 {
 	int	i;
 	int	cnt;
 
 	i = -1;
 	cnt = 0;
-	while(tmp[++i])
+	while (tmp[++i])
 		cnt++;
 	i = -1;
 	while (++i < cnt)
@@ -30,13 +30,15 @@ void	free_split(char **tmp)//ft_split으로 할당한거 해제하기
 
 void	free_split_part(char **tmp, int idx)
 {
-	int i = 0;
-	while(i<idx)
+	int	i;
+
+	i = 0;
+	while (i < idx)
 	{
 		free(tmp[i]);
 		tmp[i] = NULL;
 		i++;
 	}
 	free(tmp);
-	tmp = NULL;;
+	tmp = NULL;
 }
