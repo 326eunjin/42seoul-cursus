2d4dec7920c5aa6b128752df3afa2496550112ac
326eunjin <326eunjin@naver.com>
2022-07-13 19:56:30 +0900

    ex00 수정

--- CPP Moudle 00/ex00/megaphone.cpp
+++ CPP Moudle 00/ex00/megaphone.cpp
@@ -2,25 +2,25 @@
 #include <cctype>
 #include <cstring>
 
-void	print_in_upper_case(char *str);
+void print_in_upper_case(char *str);
 
 int main(int argc, char **argv)
 {
-	if(argc == 1)
-		std::cout<< "* LOUD AND UNBEARABLE FEEDBACK NOISE *"<< std::endl;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
 	{
-		for(int i = 1;i<argc;i++)
+		for (int i = 1; i < argc; i++)
 			print_in_upper_case(argv[i]);
-		std::cout<<std::endl;
+		std::cout << std::endl;
 	}
 }
 
-void	print_in_upper_case(char *str)
+void print_in_upper_case(char *str)
 {
 	std::string tmp;
 	std::string const line(str);
-	for(int i = 0;i<(int)line.length();i++)
+	for (int i = 0; i < (int)line.length(); i++)
 		tmp += std::toupper(line[i]);
-	std::cout<<tmp;
+	std::cout << tmp;
 }
