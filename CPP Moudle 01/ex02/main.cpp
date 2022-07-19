3086acb4a0b3ad1c4599bb4a816cdf1483315a3f
326eunjin <326eunjin@naver.com>
2022-07-19 17:27:13 +0900

    주석 추가

--- CPP Moudle 01/ex02/main.cpp
+++ CPP Moudle 01/ex02/main.cpp
@@ -4,7 +4,7 @@
 int main() {
     std::string str = "HI THIS IS BRAIN";
     std::string *stringPTR = &str;
-    std::string &stringREF = str;
+    std::string &stringREF = str; // stringREF는 str의 또 다른 이름이다!
     std::cout << "------ADDRESS--------" << std::endl;
     std::cout << "str :" << &str << std::endl;
     std::cout << "stringPTR : " << stringPTR << std::endl;
